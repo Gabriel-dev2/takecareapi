139598e549b4720386e29493a3abda235b86d76f
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-04-10 13:24:21 -0300

    API-01 Criaçao do projeto

---
+++ src/main/java/com/takecare/takecareapi/TakecareapiApplication.java
@@ -0,0 +1,13 @@
+package com.takecare.takecareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakecareapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakecareapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takecare/takecareapi/TakecareapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takecare.takecareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakecareapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
