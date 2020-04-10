9704eeb534c977cf265cda2e92bf0f99c4e56b3f
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-04-10 16:44:49 -0300

    API-01 Classe de usuario, ela e a responsavel por carregar informaçoes relacionadas aos usuarios

---
+++ src/main/java/com/takecare/takecareapi/entities/User.java
@@ -0,0 +1,22 @@
+package com.takecare.takecareapi.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String planCard;
+
+    public User(){}
+    
+}
