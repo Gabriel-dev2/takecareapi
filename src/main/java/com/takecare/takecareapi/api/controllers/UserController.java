ba2ad0acac4d017b046961c6fdf0e6736bce6936
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-04-26 22:09:35 -0300

    API-01 Adiçao de mais um endpoint para comunicaçao com o firebase

--- src/main/java/com/takecare/takecareapi/api/controllers/UserController.java
+++ src/main/java/com/takecare/takecareapi/api/controllers/UserController.java
@@ -1,15 +1,20 @@
 package com.takecare.takecareapi.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 import javax.validation.Valid;
 
 import com.takecare.takecareapi.dto.UserSubmitDTO;
 import com.takecare.takecareapi.entities.User;
+import com.takecare.takecareapi.services.FirebaseService;
 import com.takecare.takecareapi.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,20 +30,32 @@ import io.swagger.annotations.*;
 public class UserController {
 
     private UserService userService;
+    private FirebaseService firebaseService;
 
     @Autowired
-    public UserController(UserService userService){
+    public UserController(UserService userService, FirebaseService firebaseService) {
         this.userService = userService;
+        this.firebaseService = firebaseService;
     }
 
     @GetMapping(path = "/user/list")
     @ApiOperation(value = "List all users data")
-    @ApiResponses(value = {
-        @ApiResponse(code = 200, message = "A list of all users"),
-        @ApiResponse(code = 404, message = "There are no registered users")})
-        public ResponseEntity<User> listAllUsers(){
-            User u = this.userService.listAll();
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "A list of all users"),
+            @ApiResponse(code = 404, message = "There are no registered users") })
+    public ResponseEntity<User> listAllUsers() {
+        User u = this.userService.listAll();
+        return ResponseEntity.ok().body(u);
+    }
+
+    @GetMapping(path = "/user/getUser/{id}")
+    @ApiOperation(value = "Select user")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "User Found"),
+            @ApiResponse(code = 404, message = "User not found") })
+    public ResponseEntity<String> getUserbyId(@ApiParam("User Id") @Valid @PathVariable String id)
+            throws InterruptedException, ExecutionException, IOException {
+            String u = this.firebaseService.selectUserFromDb("paciente", id);
+
             return ResponseEntity.ok().body(u);
-        }
+        }   
 
 }
