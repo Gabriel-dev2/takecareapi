dd1f98023153348f3093aad977f1fb7439c604f5
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 17:31:30 -0300

    API-01 Correçao na forma de retorno do metodo na implementaçao

--- src/main/java/com/takecare/takecareapi/services/impl/UserServiceImpl.java
+++ src/main/java/com/takecare/takecareapi/services/impl/UserServiceImpl.java
@@ -1,23 +1,36 @@
 package com.takecare.takecareapi.services.impl;
 
+import com.takecare.takecareapi.api.dto.UserResponseDTO;
 import com.takecare.takecareapi.entities.Paciente;
+import com.takecare.takecareapi.repository.PacienteRepository;
 import com.takecare.takecareapi.services.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    PacienteRepository pacienteRepo;
+
+    @Autowired
+    public UserServiceImpl(PacienteRepository p) {
+        this.pacienteRepo = p;
+    }
+
     @Override
-    public Paciente createUser(String userName, String userPassword, String userEmail, String userPlanCard) {
+    public String createUser(String userName, String cpf, String endereco, int numeroCasa) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Paciente findUserById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+    public UserResponseDTO findUserById(int id) {
+        Paciente paciente = new Paciente();
+        UserResponseDTO responseDTO = new UserResponseDTO();
+        paciente = pacienteRepo.findById(id);
+        responseDTO.setPaciente(paciente);
+        return responseDTO;
     }
 
     @Override
@@ -28,8 +41,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Paciente listAll() {
-        Paciente u = new Paciente(1, "Gabriel Lucas", "gabriel.lima@testedeemail.com.br", "abcd@12345", "55214620156");
-        return u;
+        return null;
     }
 
 }
