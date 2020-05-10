5467f35022259759420c2c74800f4a1c09f3ff80
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 17:35:48 -0300

    API-02 Criaçao do serviço de autenticaçao

---
+++ src/main/java/com/takecare/takecareapi/services/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.takecare.takecareapi.services;
+
+public interface AuthenticationService {
+
+    /*
+    TODO:  Implementar serviço de autenticação após tabela ser criada
+    */
+    
+}
