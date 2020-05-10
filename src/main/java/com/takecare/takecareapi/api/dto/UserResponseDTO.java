8d3d2724d88bd395ccd98d6d9bc995d0ee648992
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 17:38:59 -0300

    API-02 Template de Response para operaçoes do serviço usuario

--- src/main/java/com/takecare/takecareapi/api/dto/UserResponseDTO.java
+++ src/main/java/com/takecare/takecareapi/api/dto/UserResponseDTO.java
@@ -1,8 +1,10 @@
 package com.takecare.takecareapi.api.dto;
 
+import com.takecare.takecareapi.entities.Paciente;
+
 import lombok.Data;
 
 @Data
 public class UserResponseDTO {
-    private String message;
+    private Paciente paciente;
 }
