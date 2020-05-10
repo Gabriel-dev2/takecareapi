86d10269debd041be3aa262bc2ce0e82779bcc05
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 17:37:01 -0300

    API-02 Classe do tipo interface do repositorio paciente

---
+++ src/main/java/com/takecare/takecareapi/repository/PacienteRepository.java
@@ -0,0 +1,13 @@
+package com.takecare.takecareapi.repository;
+
+import java.util.List;
+
+import com.takecare.takecareapi.entities.Paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    List<Paciente> findAll();
+
+    Paciente findById(int Id);
+}
