80ed5718000cdf16033f62fc78d1fbf43ee852e3
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 17:38:11 -0300

    API-02 Classe da entidade do banco de dados representado a tabela paciente

--- src/main/java/com/takecare/takecareapi/entities/Paciente.java
+++ src/main/java/com/takecare/takecareapi/entities/Paciente.java
@@ -1,17 +1,17 @@
 package com.takecare.takecareapi.entities;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
-@Table(name = "Paciente")
+@Table(name = "paciente")
 
 @Data
 @AllArgsConstructor
@@ -29,6 +29,22 @@ public class Paciente {
 
     @Column(name = "endereco")
     private String endereco;
+
+    @ManyToOne
+    @JoinColumn(name="id_planodesaude")
+    private PlanoSaude plano;
+
+    @Column(name = "numerocasa")
+    private int numeroCasa;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "pais")
+    private String pais;
 
     @Column(name = "cep")
     private String cep;
