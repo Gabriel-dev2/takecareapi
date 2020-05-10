86266433e806e6bfffc15009b93534beb4af2502
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 17:40:41 -0300

    API-02 Controller para serviço de autenticaçao

--- src/main/java/com/takecare/takecareapi/api/controllers/AuthController.java
+++ src/main/java/com/takecare/takecareapi/api/controllers/AuthController.java
@@ -1,5 +1,14 @@
 package com.takecare.takecareapi.api.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@RequestMapping(path = "api/login", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(value = "Auth", tags = "Authenticator", description = "Endpoint for authentication")
 public class AuthController {
     
 }
