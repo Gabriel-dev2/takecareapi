0efbcbd686fd9a5fb6c5dd25e03567b46c0eae97
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-10 12:26:46 -0300

    API-01 Remoçao de imports e implementaçoes do firebase

--- src/main/java/com/takecare/takecareapi/api/controllers/UserController.java
+++ src/main/java/com/takecare/takecareapi/api/controllers/UserController.java
@@ -1,28 +1,26 @@
 package com.takecare.takecareapi.api.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 import javax.validation.Valid;
 
-import com.takecare.takecareapi.dto.UserSubmitDTO;
-import com.takecare.takecareapi.entities.User;
-import com.takecare.takecareapi.services.FirebaseService;
+import com.takecare.takecareapi.entities.Paciente;
 import com.takecare.takecareapi.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -30,20 +28,18 @@ import io.swagger.annotations.*;
 public class UserController {
 
     private UserService userService;
-    private FirebaseService firebaseService;
 
     @Autowired
-    public UserController(UserService userService, FirebaseService firebaseService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.firebaseService = firebaseService;
     }
 
     @GetMapping(path = "/user/list")
     @ApiOperation(value = "List all users data")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "A list of all users"),
             @ApiResponse(code = 404, message = "There are no registered users") })
-    public ResponseEntity<User> listAllUsers() {
-        User u = this.userService.listAll();
+    public ResponseEntity<Paciente> listAllUsers() {
+        Paciente u = this.userService.listAll();
         return ResponseEntity.ok().body(u);
     }
 
@@ -53,9 +49,8 @@ public class UserController {
             @ApiResponse(code = 404, message = "User not found") })
     public ResponseEntity<String> getUserbyId(@ApiParam("User Id") @Valid @PathVariable String id)
             throws InterruptedException, ExecutionException, IOException {
-            String u = this.firebaseService.selectUserFromDb("paciente", id);
 
-            return ResponseEntity.ok().body(u);
+            return ResponseEntity.ok().body("u");
         }   
 
 }
