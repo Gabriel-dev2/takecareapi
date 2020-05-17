79710f040814ecb56d3b1b0806b5cbe22848b7e2
Gabriel <gabriel.lima@neurotech.com.br>
2020-05-16 21:55:56 -0300

    API-02 Implementaçao do endpoint de login

--- src/main/java/com/takecare/takecareapi/api/controllers/AuthController.java
+++ src/main/java/com/takecare/takecareapi/api/controllers/AuthController.java
@@ -1,14 +1,42 @@
 package com.takecare.takecareapi.api.controllers;
 
+import com.takecare.takecareapi.api.dto.LoginResponseDTO;
+import com.takecare.takecareapi.api.dto.SubmitLoginRequestDTO;
+import com.takecare.takecareapi.services.AuthenticationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
 
 @RestController
-@RequestMapping(path = "api/login", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "api/auth", produces = MediaType.APPLICATION_JSON_VALUE)
 @Api(value = "Auth", tags = "Authenticator", description = "Endpoint for authentication")
 public class AuthController {
+
+    private AuthenticationService auth;
+
+    @Autowired
+    public AuthController(AuthenticationService auth){
+        this.auth = auth;
+    }
     
+    @PostMapping(path = "/paciente/login")
+    @ApiOperation(value = "Authenticate User")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 401, message = "Unauthorized")
+    })
+    public ResponseEntity<LoginResponseDTO> authenticate(@RequestBody SubmitLoginRequestDTO body){
+        LoginResponseDTO response = auth.authenticate(body);
+        return ResponseEntity.ok().body(response);
+    }
 }
