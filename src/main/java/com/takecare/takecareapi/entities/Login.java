518730120df841007cefb90099c90454e466eb0d
Gabriel <gabriel.lima@neurotech.com.br>
2020-05-16 21:53:01 -0300

    API-02 Implementaçao da entidade Login

---
+++ src/main/java/com/takecare/takecareapi/entities/Login.java
@@ -0,0 +1,34 @@
+package com.takecare.takecareapi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "login")
+
+@Data
+@AllArgsConstructor
+public class Login {
+
+    public Login() {
+	}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "senha")
+    private String senha;
+
+
+
+    
+}
