94cbf7baceda8edc55dcc44e1deba33d9baaf5b2
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-07 15:38:50 -0300

    API-02 Remoçao de import nao utilizado

--- src/main/java/com/takecare/takecareapi/services/AuthenticationService.java
+++ src/main/java/com/takecare/takecareapi/services/AuthenticationService.java
@@ -2,10 +2,10 @@ package com.takecare.takecareapi.services;
 
 import com.takecare.takecareapi.api.dto.LoginResponseDTO;
 import com.takecare.takecareapi.api.dto.SubmitLoginRequestDTO;
-import com.takecare.takecareapi.entities.Login;
 
 public interface AuthenticationService {
 
     LoginResponseDTO authenticate(SubmitLoginRequestDTO request);
+
     
 }
