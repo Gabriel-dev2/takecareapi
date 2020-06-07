7e24643385885b5ac3602038f9e0a3819d40843d
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-07 15:44:22 -0300

    API-03 Criaçao da classe Sintomas

---
+++ src/main/java/com/takecare/takecareapi/entities/Sintomas.java
@@ -0,0 +1,30 @@
+package com.takecare.takecareapi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "sintomas")
+
+
+@Data
+@AllArgsConstructor
+public class Sintomas {
+    
+    @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idsintomas")
+    private Integer idSintoma;
+
+    @Column(name = "nomesintoma")
+    private String nome;
+
+    public Sintomas(){}
+}
