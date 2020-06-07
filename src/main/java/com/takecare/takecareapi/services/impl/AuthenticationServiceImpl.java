62d5856c0e27ed00697dcf2a9a51f09eb2bba861
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-07 15:37:56 -0300

    API-02 Mudança na implementaçao do login para utilizaçao do cpf como chave primaria

--- src/main/java/com/takecare/takecareapi/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/takecare/takecareapi/services/impl/AuthenticationServiceImpl.java
@@ -1,32 +1,42 @@
 package com.takecare.takecareapi.services.impl;
 
 
+import java.util.Optional;
+
 import com.takecare.takecareapi.api.dto.LoginResponseDTO;
 import com.takecare.takecareapi.api.dto.SubmitLoginRequestDTO;
 import com.takecare.takecareapi.entities.Login;
 import com.takecare.takecareapi.repository.LoginRepository;
+import com.takecare.takecareapi.repository.PacienteRepository;
 import com.takecare.takecareapi.services.AuthenticationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    @Autowired
     LoginRepository loginRepo;
 
     @Override
     public LoginResponseDTO authenticate(SubmitLoginRequestDTO request) {
-        Login l = new Login();
+        String cpf = request.getCpf();
+        String senha = request.getSenha();
         LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
-        l = loginRepo.getOne(request.getCpf());
-        if(l.equals(null)) {
-            loginResponseDTO.setMessage("Login incorreto ou não existe");
-        } else if(l.getSenha() != request.getSenha()) {
-            loginResponseDTO.setMessage("Senha incorreta");
-        } else {
-            loginResponseDTO.setLogin(l);
-            loginResponseDTO.setMessage("OK");
+        try {
+            Optional<Login> l = loginRepo.findByCpf(cpf);
+            if(l.isPresent()){
+                if(l.equals(null)) {
+                    loginResponseDTO.setMessage("Login incorreto ou não existe");
+                } else if(!senha.equals(l.get().getSenha())) {
+                    loginResponseDTO.setMessage("Senha incorreta");
+                } else {
+                    loginResponseDTO.setCpf(l.get().getCpf());
+                    loginResponseDTO.setMessage("OK");
+                }
         }
+        } catch(Exception e) {}
 
         return loginResponseDTO;
     }
