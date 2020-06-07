b03d597bd60045a762b26745e86991497e33ea7c
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-07 15:43:32 -0300

    API-02 Substituiçao do tipo Login no template DTO do login para utilizaçao do CPF

--- src/main/java/com/takecare/takecareapi/api/dto/LoginResponseDTO.java
+++ src/main/java/com/takecare/takecareapi/api/dto/LoginResponseDTO.java
@@ -1,12 +1,11 @@
 package com.takecare.takecareapi.api.dto;
 
-import com.takecare.takecareapi.entities.Login;
 
 import lombok.Data;
 
 @Data
 public class LoginResponseDTO {
     
-    private Login login;
+    private String cpf;
     private String message;
 }
