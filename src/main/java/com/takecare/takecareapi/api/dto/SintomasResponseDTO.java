63a654d79d5be26cd35ee250bd2fc72e6f2fa0f3
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-07 15:46:31 -0300

    API-03 Implementaçao do template de response da submissao de insert do sintoma

---
+++ src/main/java/com/takecare/takecareapi/api/dto/SintomasResponseDTO.java
@@ -0,0 +1,10 @@
+package com.takecare.takecareapi.api.dto;
+
+import com.takecare.takecareapi.entities.Sintomas;
+
+import lombok.Data;
+
+@Data
+public class SintomasResponseDTO {
+    private Sintomas sintoma;
+}
