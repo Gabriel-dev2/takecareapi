9c07945c3e6fc3452dc0a226f3ed8acc1d743866
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-07 15:42:27 -0300

    API-02 Configuraçao do metodo findByCpf com import da lib Query do jpa.repository

--- src/main/java/com/takecare/takecareapi/repository/LoginRepository.java
+++ src/main/java/com/takecare/takecareapi/repository/LoginRepository.java
@@ -1,11 +1,17 @@
 package com.takecare.takecareapi.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.takecare.takecareapi.entities.Login;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LoginRepository extends JpaRepository<Login, String>{
     List<Login> findAll();
+    
+    @Query(value = "SELECT * FROM login where login.cpf =?1", nativeQuery = true)
+    Optional<Login> findByCpf(String cpf);
+    
 }
