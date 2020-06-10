dfabcc52d7350bdf98e2ad96e03d888eaf449bc1
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-09 21:15:43 -0300

    API-03 Remoçao de import nao utilizado

--- src/main/java/com/takecare/takecareapi/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/takecare/takecareapi/services/impl/AuthenticationServiceImpl.java
@@ -7,7 +7,6 @@ import com.takecare.takecareapi.api.dto.LoginResponseDTO;
 import com.takecare.takecareapi.api.dto.SubmitLoginRequestDTO;
 import com.takecare.takecareapi.entities.Login;
 import com.takecare.takecareapi.repository.LoginRepository;
-import com.takecare.takecareapi.repository.PacienteRepository;
 import com.takecare.takecareapi.services.AuthenticationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
