0187f9044ec219b20ac898b24f07dad443301cec
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-13 20:05:27 -0300

    API-04 Adiçao de anotaçao de geraçao do id autoincrement

--- src/main/java/com/takecare/takecareapi/entities/PlanoSaude.java
+++ src/main/java/com/takecare/takecareapi/entities/PlanoSaude.java
@@ -2,6 +2,8 @@ package com.takecare.takecareapi.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,6 +16,7 @@ import lombok.Data;
 public class PlanoSaude {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_planodesaude")
     private int id;
 
