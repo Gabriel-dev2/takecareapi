e26c73b654652e615eb9cd8ea164ee0f74f0b107
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-13 20:14:35 -0300

    API-05 Implementaçao de template de request para ediçao de usuario

---
+++ src/main/java/com/takecare/takecareapi/api/dto/InsertPlanoDeSaudeDTO.java
@@ -0,0 +1,12 @@
+package com.takecare.takecareapi.api.dto;
+
+import lombok.Data;
+
+@Data
+public class InsertPlanoDeSaudeDTO {
+    
+
+    private String nome;
+
+    private String categoria;
+}
