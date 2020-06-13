a03e76352f1fd90392bfcb0a012838bc8061d072
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-13 20:06:29 -0300

    API-04 Adiçao das colunas idPlano e email referentes a tabela paciente

--- src/main/java/com/takecare/takecareapi/entities/Paciente.java
+++ src/main/java/com/takecare/takecareapi/entities/Paciente.java
@@ -5,8 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -35,6 +33,12 @@ public class Paciente {
 
     @Column(name = "numerocasa")
     private int numeroCasa;
+
+    @Column(name = "id_planodesaude")
+    private int idPlano;
+
+    @Column(name = "email")
+    private String email;
 
     public Paciente(){}
     
