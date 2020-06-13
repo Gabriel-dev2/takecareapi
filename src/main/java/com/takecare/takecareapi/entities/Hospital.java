cc9bd461f4d6200b920a9362d6fddd3044e4b32b
Gabriel <gabriel.lima@neurotech.com.br>
2020-06-13 20:11:16 -0300

    API-05 Criaçao da entidade da tabela hospital

---
+++ src/main/java/com/takecare/takecareapi/entities/Hospital.java
@@ -0,0 +1,39 @@
+package com.takecare.takecareapi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "hospital")
+
+@Data
+@AllArgsConstructor
+public class Hospital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_hospital")
+    private int idHospital;
+
+    @Column(name = "nomehospital")
+    private String nomeHospital;
+
+    @Column(name = "categoria")
+    private String categoria;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "id_paciente")
+    private int idPaciente;
+
+    @Column(name = "id_planodesaude")
+    private int idPlanoDeSaude;
+}
